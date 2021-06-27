9fd82aa1716208be96aac4aaded87b7526fec5c5
krishpranav <krisna.pranav@gmail.com>
2021-06-27 13:48:47 +0530

    [+] colors [+]

--- androidransome.py
+++ androidransome.py
@@ -4,3 +4,14 @@
 import os, sys, time, fileinput
 from getpass import getpass
 from PIL import Image
+
+# colors
+r = "\033[1;31m"
+g = "\033[1;32m"
+y = "\033[1;33m"
+b = "\033[1;34m"
+d = "\033[2;37m"
+R = "\033[1;41m"
+Y = "\033[1;43m"
+B = "\033[1;44m"
+w = "\033[0m"
